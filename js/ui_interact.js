3272933110c291a93d839e8791c66d44389cd06d
Joe Polaski <polaski.joseph@gmail.com>
2020-10-31 15:16:54 -0400

    Added ui_interact.js

---
+++ js/ui_interact.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    var ROWSELECTED = null;
+
+    //SELECT ROW EVENT
+    $('table.table tbody tr').on('click',function(e){        
+
+        if($(this).hasClass('tr-selected')){
+            $(this).removeClass('tr-selected'); // add selected class
+            ROWSELECTED = null;
+        } else {
+            // check for multiple selection
+            if(ROWSELECTED === null){
+                $(this).addClass('tr-selected'); // add selected class            
+                ROWSELECTED = this;
+            } else {
+                $('#select-alert').addClass('fade-inOut');
+                setTimeout(function(){$('#select-alert').removeClass('fade-inOut');}, 2000)             
+            }               
+                     
+        }      
+
+    });
+
+});
