cba9009119b4ed04ce229567185b5094c3c9071e
Joe Polaski <polaski.joseph@gmail.com>
2020-11-22 23:59:20 -0500

    Built Add Order Lightbox for Customer Page

--- js/buildTables.js
+++ js/buildTables.js
@@ -59,14 +59,61 @@ for(let key in data['results']){
 
 }
 
+function buildAddCustOrd(data){
+
+    let rowNum = 1; // tracks row number for labeling
+    let columns = ['Product id', 'Title', 'Publisher', 'Platform', 'Genre', 'Rating', 'quantity', 'price']
+
+
+    // Setup Add Order Table
+    $('.c-ord-tbl').append('<thead class="thead-dark ord-thead"></thead>') // add table body
+
+    // add header row
+    $('.ord-thead').append('<tr id="ord-headrow"></tr>')
+
+    // Add Headings
+    for(let i = 0; i < columns.length; i++){
+        let cellString = '<th scope="col">' + columns[i] + '</th>';
+        $('#ord-headrow').append(cellString);
+    }
+
+    // Setup Add Order Table
+    $('.c-ord-tbl').append('<tbody id="table-bod-ord"></tbody>') // add table body
+
+    // BUILD ROWS AND CELLS
+    for(let key in data['results']){
+        
+        $('#table-bod-ord').append('<tr id="tr-ord-' + rowNum + '"></tr>'); // add row for each key in response data
+
+        let currentRow = Object.values(data['results'][key]); // capture current row values array
+
+        // insert cells for row
+        for(let i = 0; i < currentRow.length; i++){
+            let rowID = '#tr-ord-'+ rowNum;
+            // first item -> insert <th></th>
+            if(i == 0){
+                $(rowID).append('<th scope="row">'+ currentRow[i] +'</th>'); 
+            } else {    // other items -> insert <td></td>
+                $(rowID).append('<td>'+ currentRow[i] +'</td>');
+            }
+        }
+
+        rowNum++; // increment row number
+    }
+
+}
+
 const updateTableData = ()=>{
     // Add card data
-    //getDataFromServer(`${servURL}/fullCount`,fullCount)
+    getDataFromServer(`${servURL}/fullCount`,fullCount)
 
     // CUSTOMER PAGE
     if(customers !== -1){
         getDataFromServer(`${servURL}/allCustomers`,buildTable); // make async call  
-        setHeader(['Customer ID', 'First', 'last', 'Email']); 
+        setHeader(['Customer ID', 'First', 'last', 'Email']);
+        
+        // get all products for add order on customer page
+        getDataFromServer(`${servURL}/allProducts`,buildAddCustOrd); // make async call 
     }
 
     // CUSTOMER ADDRESSES PAGE
@@ -98,4 +145,4 @@ const updateOrderHistory = (fname, lname) => {
     setHeader(['Order id', 'Customer', 'Order Status', 'Date Ordered', 'Total Price']); 
 }
 
-export {updateTableData, updateOrderHistory};
+export {updateTableData, updateOrderHistory, buildAddCustOrd};
--- js/lightbox.js
+++ js/lightbox.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+    /****************************
+    ****** CUSTOMER PAGE**********
+    ******************************/
+
     //ADD CUSTOMER LIGHTBOX EVENT
     $('#btn-add-cust').on('click', () =>{        
         $('#cust-ltbox-bckdrp-1').removeClass('invisible');
@@ -11,6 +15,59 @@ $(document).ready(function(){
         $('#cust-ltbox-bckdrp-1').removeClass('visible');
         $('#cust-ltbox-bckdrp-1').addClass('invisible');
     });
+
+    //ADD CUSTOMER ORDER LIGHTBOX EVENT
+    $('#btn-ord-cust').on('click', () =>{        
+        $('#cust-ltbox-bckdrp-3').removeClass('invisible');
+        $('#cust-ltbox-bckdrp-3').addClass('visible');
+    });    
+
+    //ADD CUSTOMER ORDER LIGHTBOX CLOSE BUTTON
+    $('#cust-close-3').on('click', () =>{        
+        $('#cust-ltbox-bckdrp-3').removeClass('visible');
+        $('#cust-ltbox-bckdrp-3').addClass('invisible');
+    });
+
+    //ADD ITEM TO ORDER LIGHTBOX EVENT
+    $('#btn-add-ord-item').on('click', () =>{        
+        $('#form-ord-row').append(`<div class="form-group col-md-9">
+        <input type="text" class="form-control" id="order-item" placeholder="enter item name">
+      </div>
+      <div class="form-group col-md-3">
+        <input type="text" class="form-control" id="itemQuantity" placeholder="item quantity">               
+      </div>`);
+        
+    });
+
+    //ADD CUSTOMER ORDER
+    $('#btn-cust-ord').on('click', () =>{      
+        
+        // Object to hold order
+        let orderObject = {
+            itemNames: [],
+            itemQuantities: []
+        }
+
+        let keycount = 0
+
+        // Pull Data for all items
+        $('#form-ord-row > div').children().each(function(){
+            let itemValue = $(this).val()
+            
+            // check if on name or quantity
+            if(keycount % 2 == 0){
+                orderObject.itemNames.push(itemValue) // add item names to proper key 
+            } else {
+                orderObject.itemQuantities.push(itemValue) // add item quantities to proper key
+            }
+            keycount = keycount+1;           
+
+        });
+
+        console.log(orderObject)
+
+    });
+
 
     //ADD CUSTOMER ADDRESS LIGHTBOX EVENT
     $('#btn-add-cAddr').on('click', () =>{        
@@ -24,13 +81,17 @@ $(document).ready(function(){
         $('#cust-ltbox-bckdrp-2').addClass('invisible');
     });
 
-    //ADD CUSTOMER LIGHTBOX EVENT
+    /****************************
+    ****** PRODUCT PAGE**********
+    ******************************/
+
+    //ADD PRODUCT LIGHTBOX EVENT
     $('#btn-add-product-main').on('click', () =>{      
         $('#ltbox-bckdrp-prod').removeClass('invisible');
         $('#ltbox-bckdrp-prod').addClass('visible');
     });
 
-    //ADD CUSTOMER LIGHTBOX CLOSE BUTTON
+    //ADD PRODUCT LIGHTBOX CLOSE BUTTON
     $('#prod-close').on('click', () =>{        
         $('#ltbox-bckdrp-prod').removeClass('visible');
         $('#ltbox-bckdrp-prod').addClass('invisible');
